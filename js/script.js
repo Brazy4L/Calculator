542dea3e7b94549dd1f8a4631a1b452ffadbc04d
Vlad Brazy <shutn1q@gmail.com>
2022-06-12 22:06:37 +0300

    Fix flashing white screen at the loading

--- js/script.js
+++ js/script.js
@@ -4,7 +4,13 @@ const subtract = (a, b) => a - b;
 
 const multiply = (a, b) => a * b;
 
-const divide = (a, b) => a / b;
+const divide = (a, b) => {
+  if (b === '0') {
+    return 9000;
+  } else {
+    return a / b;
+  }
+};
 
 const operate = (operator, a, b) => {
   switch (operator) {
