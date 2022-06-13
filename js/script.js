b2e77bcf21b6991d7e4233fd4fee16de683c60ba
Vlad Brazy <shutn1q@gmail.com>
2022-06-13 23:59:21 +0300

    Add keyboard support

--- js/script.js
+++ js/script.js
@@ -108,3 +108,30 @@ const round = (num) => {
     return Math.round(num * 100) / 100;
   }
 };
+
+window.addEventListener('keydown', (event) => {
+  if (event.key === '1') {document.getElementById('one').click();}
+  if (event.key === '2') {document.getElementById('two').click();}
+  if (event.key === '3') {document.getElementById('three').click();}
+  if (event.key === '4') {document.getElementById('four').click();}
+  if (event.key === '5') {document.getElementById('five').click();}
+  if (event.key === '6') {document.getElementById('six').click();}
+  if (event.key === '7') {document.getElementById('seven').click();}
+  if (event.key === '8') {document.getElementById('eight').click();}
+  if (event.key === '9') {document.getElementById('nine').click();}
+  if (event.key === '0') {document.getElementById('zero').click();}
+  if (event.key === '+') {document.getElementById('add').click();}
+  if (event.key === '-') {document.getElementById('subtract').click();}
+  if (event.key === '*') {document.getElementById('multiply').click();}
+  if (event.key === 'x') {document.getElementById('multiply').click();}
+  if (event.key === '/') {document.getElementById('divide').click();}
+  if (event.key === '%') {document.getElementById('percentage').click();}
+  if (event.key === 'Enter') {document.getElementById('equals').click();}
+  if (event.key === '=') {document.getElementById('equals').click();}
+  if (event.key === 'Backspace') {document.getElementById('backspace').click();}
+  if (event.key === 'Delete') {document.getElementById('all-clear').click();}
+  if (event.key === 'Escape') {document.getElementById('all-clear').click();}
+  if (event.key === 'c') {document.getElementById('all-clear').click();}
+  if (event.key === '.') {document.getElementById('decimal').click();}
+  if (event.key === ',') {document.getElementById('decimal').click();}
+});
